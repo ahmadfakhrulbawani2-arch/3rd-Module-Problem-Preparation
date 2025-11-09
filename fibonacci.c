0181ebc8e50cfa572396d6421972e1930ae1922f
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-09 20:29:45 +0700

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fibb(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return fibb(n-1) + fibb(n-2);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    int target = fibb(n-1);
+    printf("%d\n", target);
+}
